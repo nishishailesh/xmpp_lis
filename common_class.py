158f0dfbac5654dc45d4411e869065f2cb21274f
root <biochemistrygmcs@gmail.com>
2021-10-14 18:06:09 +0530

    new file:   send_ca.py 	new file:   send_client.py 	new file:   staff.conf.example 	new file:   update_xmpp_lis.sh 	new file:   x.py

---
+++ common_class.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+import os, fcntl,shutil,datetime, logging, sys, os
+
+def print_to_log(object1,object2):
+  logging.debug('{} {}'.format(object1,object2))
+
+def set_configuration_file_path():  
+  #setup folder path of configuration file
+  this_file=os.path.abspath(__file__)
+  #print('setting path',this_file)
+  parent_folder=os.path.dirname(this_file)
+  #print('setting path',parent_folder)
+  parent_of_parent_folder=os.path.dirname(parent_folder)
+  #print('setting path',parent_of_parent_folder)
+  conf_folder=parent_of_parent_folder+'/conf'
+  #print ('setting path',conf_folder)
+  sys.path.append(conf_folder)
+
+set_configuration_file_path()
+import MySQLdb
+
+class my_sql(object):
+  def get_link(self,my_host,my_user,my_pass,my_db):
+    con=MySQLdb.connect(my_host,my_user,my_pass,my_db)
+    logging.debug(con)
+    if(con==None):
+      if(debug==1): logging.debug("Can't connect to database")
+    else:
+      pass
+      logging.debug('connected')
+      return con
+
+  def run_query(self,con,prepared_sql,data_tpl):
+    cur=con.cursor()
+    cur.execute(prepared_sql,data_tpl)
+    con.commit()
+    msg="rows affected: {}".format(cur.rowcount)
+    logging.debug(msg)
+    return cur
+
+  def get_single_row(self,cur):
+    return cur.fetchone()
+
+  def close_cursor(self,cur):
+    cur.close()
+
+  def close_link(self,con):
+    con.close()
+
+#logging defined in child class, illogical
+class file_mgmt(object):
+  def __init__(self):
+    pass
+    
+  def set_inbox(self,inbox_data,inbox_arch):
+    self.inbox_data=inbox_data
+    self.inbox_arch=inbox_arch
+    
+
+  def set_outbox(self,outbox_data,outbox_arch):
+    self.outbox_data=outbox_data
+    self.outbox_arch=outbox_arch
+
+  def get_first_inbox_file(self):
+    self.current_inbox_file=''
+    inbox_files=os.listdir(self.inbox_data)
+    for each_file in inbox_files:
+      if(os.path.isfile(self.inbox_data+each_file)):
+        self.current_inbox_file=each_file
+        print_to_log('current inbox filepath:',self.inbox_data+self.current_inbox_file)
+        try:
+          fh=open(self.inbox_data+self.current_inbox_file,'rb')
+          fcntl.flock(fh, fcntl.LOCK_EX | fcntl.LOCK_NB)
+          return True
+        except Exception as my_ex:
+         msg="{} is locked. trying next..".format(self.inbox_data+self.current_inbox_file)
+         print_to_log(my_ex,msg)
+    return False  #no file to read
+
+
+  def get_first_outbox_file(self):
+    self.current_outbox_file=''
+    outbox_files=os.listdir(self.outbox_data)
+    for each_file in outbox_files:
+      if(os.path.isfile(self.outbox_data+each_file)):
+        self.current_outbox_file=each_file
+        print_to_log('current outbox filepath:',self.outbox_data+self.current_outbox_file)
+        try:
+          fh=open(self.outbox_data+self.current_outbox_file,'rb')
+          fcntl.flock(fh, fcntl.LOCK_EX | fcntl.LOCK_NB)
+          return True
+        except Exception as my_ex:
+         msg="{} is locked. trying next..".format(self.outbox_data+self.current_outbox_file)
+         print_to_log(my_ex,msg)
+    return False  #no file to read
+
+  def get_inbox_filename(self):
+    dt=datetime.datetime.now()
+    return self.inbox_data+dt.strftime("%Y-%m-%d-%H-%M-%S-%f")
+
+  def get_outbox_filename(self):
+    dt=datetime.datetime.now()
+    return self.outbox_data+dt.strftime("%Y-%m-%d-%H-%M-%S-%f")
+
+
+  def archive_outbox_file(self):
+    shutil.move(self.outbox_data+self.current_outbox_file, self.outbox_arch+self.current_outbox_file)
+    #pass #useful during debugging
+    #full path from source to destination prevent exception on ovewriting
+
+  def archive_inbox_file(self):
+    shutil.move(self.inbox_data+self.current_inbox_file, self.inbox_arch+self.current_inbox_file)
+
+
+
---
+++ send_ca.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+# Slixmpp: The Slick XMPP Library
+# Copyright (C) 2010  Nathanael C. Fritz
+# This file is part of Slixmpp.
+# See the file LICENSE for copying permission.
+
+import logging, sys
+from getpass import getpass
+from argparse import ArgumentParser
+from common_class import my_sql
+import slixmpp
+
+#For mysql password
+sys.path.append('/var/gmcs_config')
+import astm_var
+
+class SendMsgBot(slixmpp.ClientXMPP):
+
+    """
+    A basic Slixmpp bot that will log in, send a message,
+    and then log out.
+    """
+
+    def __init__(self, jid, password, recipient, message):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+
+        # The message we wish to send, and the JID that
+        # will receive it.
+        self.recipient = recipient
+        self.msg = message
+
+        # The session_start event will be triggered when
+        # the bot establishes its connection with the server
+        # and the XML streams are ready for use. We want to
+        # listen for this event so that we we can initialize
+        # our roster.
+        self.add_event_handler("session_start", self.start)
+
+    async def start(self, event):
+        """
+        Process the session_start event.
+
+        Typical actions for the session_start event are
+        requesting the roster and broadcasting an initial
+        presence stanza.
+
+        Arguments:
+            event -- An empty dictionary. The session_start
+                     event does not provide any additional
+                     data.
+        """
+        self.send_presence()
+        await self.get_roster()
+
+        self.send_message(mto=self.recipient,
+                          mbody=self.msg,
+                          mtype='chat')
+
+        self.disconnect()
+
+
+if __name__ == '__main__':
+    # Setup logging.
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(levelname)-8s %(message)s')
+
+    m=my_sql()
+    con=m.get_link(astm_var.my_host,astm_var.my_user,astm_var.my_pass,astm_var.my_db)
+    cur=m.run_query(con,'select * from im_message where message_status=0',());
+    dt=m.get_single_row(cur);
+    while(dt):
+      print(dt)
+      xmpp = SendMsgBot(astm_var.xmpp_user, astm_var.xmpp_pass, dt[1], '{}'.format(dt[2]))
+      xmpp.register_plugin('xep_0030') # Service Discovery
+      xmpp.register_plugin('xep_0199') # XMPP Ping
+
+      # Connect to the XMPP server and start processing XMPP stanzas.
+      xmpp.connect()
+      xmpp.process(forever=False)
+
+      update_cur=m.run_query(con,'update im_message set message_status=1 where id=%s',(dt[0],))
+      dt=m.get_single_row(cur);
+
+
+    m.close_cursor(cur)
+    m.close_link(con)
